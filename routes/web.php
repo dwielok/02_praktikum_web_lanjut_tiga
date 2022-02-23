2a92cf512038635b7247d83e17c7dc55ec03618e
dwielok <dwieloknuraini773@gmail.com>
2022-02-23 11:14:00 +0700

    praktikum 3 no 2 route prefix

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     echo "Ini adalah Home";
 });
+
+Route::prefix('category')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        echo "Ini halaman Marbel Edu Games";
+    });
+    Route::get('/marbel-and-friends-kids-games', function () {
+        echo "Ini halaman Marbel And Friends Kids Games";
+    });
+    Route::get('/riri-story-books', function () {
+        echo "Ini halaman Riri Story Books";
+    });
+    Route::get('/kolak-kids-songs', function () {
+        echo "Ini halaman Kolak Kids Songs";
+    });
+});
